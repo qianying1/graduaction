e854c225cf62b017dbe4daa0b658549da3e7541c
qianying1 <1101627399@qq.com>
2017-10-23 16:49:21 +0800

    10/23 first commit

---
+++ grad-parent/grad-manager/grad-manager-service/src/main/java/com/grad/BaseUtil.java
@@ -0,0 +1,10 @@
+package com.grad;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class BaseUtil {
+
+	public final Log log=LogFactory.getLog(this.getClass());
+	
+}
--- grad-parent/grad-manager/grad-manager-service/src/main/java/com/grad/support/acfunc/IndexAnalizer.java
+++ grad-parent/grad-manager/grad-manager-service/src/main/java/com/grad/support/acfunc/IndexAnalizer.java
@@ -2,14 +2,32 @@ package com.grad.support.acfunc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class IndexAnalizer {
-	
-	public List<String> analizeHeaderForUrls(Element header){
+import com.grad.BaseUtil;
+import com.grad.util.Validation;
+
+public class IndexAnalizer extends BaseUtil{
+
+	/**
+	 * 分析页面头部获取链接列表
+	 * @param header
+	 * @return
+	 */
+	public List<String> analizeHeaderForUrls(Element header) {
+		if (Validation.isNull(header) || Validation.isEmpty(header.getElementsByTag("nav"))) {
+			log.error("the header content is empty");
+			return null;
+		}
 		Element nav = header.getElementById("nav");
 		Elements ahrefs = nav.select("a");
+		if(Validation.isEmpty(ahrefs)){
+			log.error("the header don`t have super link");
+			return null;
+		}
 		List<String> urls = new ArrayList<String>();
 		for (Element ahref : ahrefs) {
 			String url = ahref.attr("abs:href");
@@ -19,6 +37,24 @@ public class IndexAnalizer {
 		}
 		return urls;
 	}
+
+	/**
+	 * 分析页面中带有轮播的section节点
+	 * @param section
+	 * @return
+	 */
+	public Map<String, Object> analizeCarouselSection(Element section){
+		
+		return null;
+	}
 	
-	public 
+	/**
+	 * 分析页面中普通的section节点
+	 * @param section
+	 * @return
+	 */
+	public Map<String, Object> analizeSection(Element section) {
+
+		return null;
+	}
 }
